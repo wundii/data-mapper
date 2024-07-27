74dfa13f8647cfc872da84414ab5d467166d2be5
awu <andreas.wunderwald@westpress.de>
2024-07-27 07:21:05 +0200

    phplint github action fix

--- phplint.php
+++ phplint.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use PHPLint\Config\LintConfig;
 
 return static function (LintConfig $lintConfig): void {
+    $lintConfig->cacheDirectory('cache/phplint');
     $lintConfig->paths([
         'src',
         'tests',
