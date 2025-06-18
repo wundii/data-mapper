00e2d044cdd14d5d2f4bd35d30306decca240285
awu <andreas.wunderwald@westpress.de>
2025-06-18 12:13:06 +0200

    rename DataMapperSymfony class to DataMapper and adjust inheritance from BaseDataMapper

--- src/Framework/Symfony/DataMapper.php
+++ src/Framework/Symfony/DataMapper.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Wundii\DataMapper\Framework;
 
 use Symfony\Component\HttpFoundation\Request;
-use Wundii\DataMapper\DataMapper;
+use Wundii\DataMapper\DataMapper as BaseDataMapper;
 use Wundii\DataMapper\Enum\SourceTypeEnum;
 use Wundii\DataMapper\Exception\DataMapperException;
 
@@ -13,7 +13,7 @@ use Wundii\DataMapper\Exception\DataMapperException;
  * @template T of object
  * @extends DataMapper<T>
  */
-class DataMapperSymfony extends DataMapper
+class DataMapper extends BaseDataMapper
 {
     /**
      * @param class-string<T>|T $object
