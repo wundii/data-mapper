b13b044a36f8d2700893523f48f82b0b4339b70d
awu <andreas.wunderwald@westpress.de>
2025-06-27 06:17:29 +0200

    test: enhance ObjectDtoTest with additional property and method assertions

--- tests/Unit/Dto/ObjectDtoTest.php
+++ tests/Unit/Dto/ObjectDtoTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Unit\Dto;
 
 use PHPUnit\Framework\TestCase;
+use Wundii\DataMapper\Dto\AnnotationDto;
 use Wundii\DataMapper\Dto\AttributeDto;
 use Wundii\DataMapper\Dto\MethodDto;
+use Wundii\DataMapper\Dto\ParameterDto;
 use Wundii\DataMapper\Dto\PropertyDto;
 use Wundii\DataMapper\Dto\ReflectionObjectDto;
 use Wundii\DataMapper\Enum\AccessibleEnum;
@@ -38,7 +40,23 @@ class ObjectDtoTest extends TestCase
             [
                 new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyConst', DataTypeEnum::STRING, 'target1', true),
                 new PropertyDto(AccessibleEnum::PROTECTED, 'dataPropertyConst', DataTypeEnum::ARRAY, 'target2', false),
-                new PropertyDto(AccessibleEnum::PRIVATE, 'itemPropertyConst', 'MockClasses\ItemConstructor', 'target3', false),
+                new PropertyDto(
+                    AccessibleEnum::PRIVATE,
+                    'itemPropertyConst',
+                    'MockClasses\ItemConstructor',
+                    'target3',
+                    false,
+                    isDefaultValueAvailable: true,
+                    defaultValue: 'default',
+                    annotationDto: new AnnotationDto([new ParameterDto('target', ['string'])], [
+                        'target' => 'itemAttributeSource',
+                    ]),
+                    attributes: [
+                        new AttributeDto(AttributeOriginEnum::TARGET_PROPERTY, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\SourceData', [
+                            'target' => 'itemAttributeSource',
+                        ]),
+                    ]
+                ),
             ],
             [
                 new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'nameGetter', DataTypeEnum::STRING, null, true),
@@ -78,7 +96,7 @@ class ObjectDtoTest extends TestCase
         $this->assertNull($object->findElementDto(ApproachEnum::SETTER, 'ITEMSETTER'));
     }
 
-    public function testFind(): void
+    public function testPropertiesClass(): void
     {
         $object = $this->objectComplex();
 
@@ -88,27 +106,136 @@ class ObjectDtoTest extends TestCase
             new PropertyDto(AccessibleEnum::PRIVATE, 'itemPropertyClass', 'MockClasses\ItemConstructor', 'target3', false),
         ];
         $this->assertEquals($expectedProperties, $object->getPropertiesClass());
+    }
+
+    public function testPropertiesConst(): void
+    {
+        $object = $this->objectComplex();
 
         $expectedProperties = [
             new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyConst', DataTypeEnum::STRING, 'target1', true),
             new PropertyDto(AccessibleEnum::PROTECTED, 'dataPropertyConst', DataTypeEnum::ARRAY, 'target2', false),
-            new PropertyDto(AccessibleEnum::PRIVATE, 'itemPropertyConst', 'MockClasses\ItemConstructor', 'target3', false),
+            new PropertyDto(
+                AccessibleEnum::PRIVATE,
+                'itemPropertyConst',
+                'MockClasses\ItemConstructor',
+                'target3',
+                false,
+                isDefaultValueAvailable: true,
+                defaultValue: 'default',
+                annotationDto: new AnnotationDto([new ParameterDto('target', ['string'])], [
+                    'target' => 'itemAttributeSource',
+                ]),
+                attributes: [
+                    new AttributeDto(AttributeOriginEnum::TARGET_PROPERTY, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\SourceData', [
+                        'target' => 'itemAttributeSource',
+                    ]),
+                ]
+            ),
         ];
         $this->assertEquals($expectedProperties, $object->getPropertiesConst());
+    }
+
+    public function testProperties(): void
+    {
+        $object = $this->objectComplex();
 
         $expectedProperties = [
+            new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyClass', DataTypeEnum::STRING, 'target1', true),
+            new PropertyDto(AccessibleEnum::PROTECTED, 'dataPropertyClass', DataTypeEnum::ARRAY, 'target2', false),
+            new PropertyDto(AccessibleEnum::PRIVATE, 'itemPropertyClass', 'MockClasses\ItemConstructor', 'target3', false),
+            new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyConst', DataTypeEnum::STRING, 'target1', true),
+            new PropertyDto(AccessibleEnum::PROTECTED, 'dataPropertyConst', DataTypeEnum::ARRAY, 'target2', false),
+            new PropertyDto(
+                AccessibleEnum::PRIVATE,
+                'itemPropertyConst',
+                'MockClasses\ItemConstructor',
+                'target3',
+                false,
+                isDefaultValueAvailable: true,
+                defaultValue: 'default',
+                annotationDto: new AnnotationDto([new ParameterDto('target', ['string'])], [
+                    'target' => 'itemAttributeSource',
+                ]),
+                attributes: [
+                    new AttributeDto(AttributeOriginEnum::TARGET_PROPERTY, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\SourceData', [
+                        'target' => 'itemAttributeSource',
+                    ]),
+                ]
+            ),
+        ];
+        $this->assertEquals($expectedProperties, $object->getProperties());
+
+        $first = array_key_first($object->getProperties());
+        $last = array_key_last($object->getProperties());
+
+        $firstProperty = $object->getProperties()[$first];
+        $lastProperty = $object->getProperties()[$last];
+
+        $this->assertFalse($firstProperty->isDefaultValueAvailable());
+        $this->assertNull($firstProperty->getDefaultValue());
+        $this->assertNull($firstProperty->getAnnotationDto());
+        $this->assertTrue($lastProperty->isDefaultValueAvailable());
+        $this->assertSame('default', $lastProperty->getDefaultValue());
+        $this->assertInstanceOf(AnnotationDto::class, $lastProperty->getAnnotationDto());
+    }
+
+    public function testAttributesClass(): void
+    {
+        $object = $this->objectComplex();
+
+        $expectedAttributes = [
             new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor1', 'MockClasses\ObjectAttribute', []),
             new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor2', 'MockClasses\ObjectAttribute', []),
             new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor3', 'MockClasses\ObjectAttribute', []),
         ];
-        $this->assertEquals($expectedProperties, $object->getAttributesClass());
+        $this->assertEquals($expectedAttributes, $object->getAttributesClass());
+    }
 
-        $expectedMethodOther = [
+    public function testAttributes(): void
+    {
+        $object = $this->objectComplex();
+
+        $expectedAttributes = [
+            new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor1', 'MockClasses\ObjectAttribute', []),
+            new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor2', 'MockClasses\ObjectAttribute', []),
+            new AttributeDto(AttributeOriginEnum::TARGET_CLASS, 'MockClasses\ItemConstructor3', 'MockClasses\ObjectAttribute', []),
+            new AttributeDto(AttributeOriginEnum::TARGET_PROPERTY, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\SourceData', [
+                'target' => 'itemAttributeSource',
+            ]),
+            new AttributeDto(AttributeOriginEnum::TARGET_METHOD, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\TargetData', [
+                'alias' => 'itemAttributeTarget',
+            ]),
+        ];
+        $this->assertEquals($expectedAttributes, $object->getAttributes());
+
+        $first = array_key_first($object->getAttributes());
+        $last = array_key_last($object->getAttributes());
+
+        $firstAttribute = $object->getAttributes()[$first];
+        $lastAttribute = $object->getAttributes()[$last];
+
+        $this->assertSame(AttributeOriginEnum::TARGET_CLASS, $firstAttribute->getAttributeOriginEnum());
+        $this->assertSame('MockClasses\ItemConstructor1', $firstAttribute->getOriginName());
+        $this->assertSame(AttributeOriginEnum::TARGET_METHOD, $lastAttribute->getAttributeOriginEnum());
+        $this->assertSame('MockClasses\ItemConstructor', $lastAttribute->getOriginName());
+    }
+
+    public function testMethodOther(): void
+    {
+        $object = $this->objectComplex();
+
+        $expectedMethods = [
             new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'nameConstructor', DataTypeEnum::STRING, null, true),
             new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'dataConstructor', DataTypeEnum::ARRAY, null, false),
             new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'itemConstructor', 'MockClasses\ItemConstructor', null, false),
         ];
-        $this->assertEquals($expectedMethodOther, $object->getMethodOther());
+        $this->assertEquals($expectedMethods, $object->getMethodOther());
+    }
+
+    public function testMethodGetters(): void
+    {
+        $object = $this->objectComplex();
 
         $expectedGetters = [
             new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'nameGetter', DataTypeEnum::STRING, null, true),
@@ -116,6 +243,11 @@ class ObjectDtoTest extends TestCase
             new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'itemGetter', 'MockClasses\ItemConstructor', null, false),
         ];
         $this->assertEquals($expectedGetters, $object->getMethodGetters());
+    }
+
+    public function testMethodSetters(): void
+    {
+        $object = $this->objectComplex();
 
         $expectedSetters = [
             new MethodDto(MethodTypeEnum::SETTER, AccessibleEnum::PUBLIC, 'nameSetter', DataTypeEnum::STRING, null, true),
@@ -127,6 +259,33 @@ class ObjectDtoTest extends TestCase
             ]),
         ];
         $this->assertEquals($expectedSetters, $object->getMethodSetters());
+    }
+
+    public function testMethods(): void
+    {
+        $object = $this->objectComplex();
+
+        $expectedMethods = [
+            new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'nameGetter', DataTypeEnum::STRING, null, true),
+            new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'dataGetter', DataTypeEnum::ARRAY, null, false),
+            new MethodDto(MethodTypeEnum::GETTER, AccessibleEnum::PUBLIC, 'itemGetter', 'MockClasses\ItemConstructor', null, false),
+            new MethodDto(MethodTypeEnum::SETTER, AccessibleEnum::PUBLIC, 'nameSetter', DataTypeEnum::STRING, null, true),
+            new MethodDto(MethodTypeEnum::SETTER, AccessibleEnum::PUBLIC, 'dataSetter', DataTypeEnum::ARRAY, null, false),
+            new MethodDto(MethodTypeEnum::SETTER, AccessibleEnum::PUBLIC, 'itemSetter', 'MockClasses\ItemConstructor', null, false, attributes: [
+                new AttributeDto(AttributeOriginEnum::TARGET_METHOD, 'MockClasses\ItemConstructor', 'Wundii\DataMapper\Attribute\TargetData', [
+                    'alias' => 'itemAttributeTarget',
+                ]),
+            ]),
+            new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'nameConstructor', DataTypeEnum::STRING, null, true),
+            new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'dataConstructor', DataTypeEnum::ARRAY, null, false),
+            new MethodDto(MethodTypeEnum::OTHER, AccessibleEnum::PUBLIC, 'itemConstructor', 'MockClasses\ItemConstructor', null, false),
+        ];
+        $this->assertEquals($expectedMethods, $object->getMethods());
+    }
+
+    public function testAvailableData(): void
+    {
+        $object = $this->objectComplex();
 
         $expectedAvailableData = [
             'namePropertyClass' => new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyClass', DataTypeEnum::STRING, 'target1', true),
@@ -136,6 +295,11 @@ class ObjectDtoTest extends TestCase
             'namePropertyConst' => new PropertyDto(AccessibleEnum::PUBLIC, 'namePropertyConst', DataTypeEnum::STRING, 'target1', true),
         ];
         $this->assertEquals($expectedAvailableData, $object->availableData());
+    }
+
+    public function testFindElementDto(): void
+    {
+        $object = $this->objectComplex();
 
         $this->assertInstanceOf(PropertyDto::class, $object->findElementDto(ApproachEnum::PROPERTY, 'namePropertyClass'));
         $this->assertSame('namePropertyClass', $object->findElementDto(ApproachEnum::PROPERTY, 'namePropertyClass')?->getName());
